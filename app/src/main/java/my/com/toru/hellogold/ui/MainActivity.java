58f965197502bb8f22a6e9bb92f0c4f2a088feed
Toru Choi <toru.choi@11street.my>
2018-09-12 11:44:06 +0800

    swiperefresh layout

--- app/src/main/java/my/com/toru/hellogold/ui/MainActivity.java
+++ app/src/main/java/my/com/toru/hellogold/ui/MainActivity.java
@@ -3,6 +3,7 @@ package my.com.toru.hellogold.ui;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -22,10 +23,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final SwipeRefreshLayout swipeRefresh = findViewById(R.id.swipe_main);
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                List<Data> dataList = AppDatabase.getAppDatabase(MainActivity.this).dataDao().getAll();
+                Log.w("InitRouter", "==============================");
+                for(Data data : dataList){
+                    Log.w("InitRouter", "data:: " + data.toString());
+                }
+                Log.w("InitRouter", "==============================");
+
+                RetrofitInitializer.getinstance().createSpotPrice(new RetrofitInitializer.OnCallback<SpotPrice>() {
+                    @Override
+                    public void onReturn(SpotPrice body) {
+                        AppDatabase.getAppDatabase(MainActivity.this).dataDao().insertEachData(body.getData());
+                        swipeRefresh.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void onNeedCheck(SpotPrice body) {
+                        swipeRefresh.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void onFailed() {
+                        swipeRefresh.setRefreshing(false);
+                    }
+                });
+            }
+        });
+
+        swipeRefresh.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light
+        );
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,14 +79,20 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onReturn(SpotPrice body) {
                         AppDatabase.getAppDatabase(MainActivity.this).dataDao().insertEachData(body.getData());
+                        swipeRefresh.setRefreshing(false);
                     }
 
                     @Override
-                    public void onNeedCheck(SpotPrice body) {}
+                    public void onNeedCheck(SpotPrice body) {
+                        swipeRefresh.setRefreshing(false);
+                    }
 
                     @Override
-                    public void onFailed() {}
+                    public void onFailed() {
+                        swipeRefresh.setRefreshing(false);
+                    }
                 });
+                swipeRefresh.setRefreshing(true);
             }
         });
     }
